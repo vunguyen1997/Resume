fae3c85fdeaee9741a79bc9eb85be5e07b439ef5
Vu Nguyen <36459079+vunguyen1997@users.noreply.github.com>
2024-11-24 17:51:44 -0700

    1st

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
